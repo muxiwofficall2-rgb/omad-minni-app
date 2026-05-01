ee7da30245b444a84a261f7fc57c90eed240a7f3
muxiwofficall2-rgb <muxiwofficall2@gmail.com>
2026-05-01 18:10:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,27 @@ function tg(user){
   window.open("https://t.me/" + user.replace("@",""));
 }
 
+// 🔥 TIL ALMASHTIRISH
 function setLang(lang){
-  if(lang === 'ru'){
-    document.getElementById('subtitle').innerText = "Сервис";
-  }else{
-    document.getElementById('subtitle').innerText = "Servis";
+
+  if(lang === "ru"){
+    document.getElementById("btn1").innerText = "✈️ Бронирование авиабилета";
+    document.getElementById("btn2").innerText = "🛂 Ответы по визе";
+    document.getElementById("btn3").innerText = "👥 Телеграм группа";
+    document.getElementById("btn4").innerText = "📞 Поддержка";
+    document.getElementById("btn5").innerText = "📝 Заполнение анкеты";
+    document.getElementById("btn6").innerText = "📄 Наши услуги";
+    document.getElementById("subtitle").innerText = "Сервис";
   }
+
+  else{
+    document.getElementById("btn1").innerText = "✈️ Aviabilet bron qilish";
+    document.getElementById("btn2").innerText = "🛂 Viza javoblari";
+    document.getElementById("btn3").innerText = "👥 Telegram guruh";
+    document.getElementById("btn4").innerText = "📞 Yordam";
+    document.getElementById("btn5").innerText = "📝 Anketa to‘ldirish";
+    document.getElementById("btn6").innerText = "📄 Uslugyalarimiz";
+    document.getElementById("subtitle").innerText = "Servis";
+  }
+
 }
